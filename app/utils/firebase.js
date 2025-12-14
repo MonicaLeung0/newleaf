521e7c43e47ffb9102018451ec13c0a5868a0158
Monica Leung <Manki.Leung@edu.sait.ca>
2025-12-13 17:17:27 -0700

    add a bit comment

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -21,3 +21,11 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+// can use this for the firebase config if you need to 
+// NEXT_PUBLIC_FIREBASE_API_KEY="AIzaSyBRm0wMPenemuySTLWJm0FHQH4JaejQn8s"
+// NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN="newleaf-ca19f.firebaseapp.com"
+// NEXT_PUBLIC_FIREBASE_PROJECT_ID="newleaf-ca19f"
+// NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET="newleaf-ca19f.firebasestorage.app"
+// NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID="81640098190"
+// NEXT_PUBLIC_FIREBASE_APP_ID="1:81640098190:web:062723de757e2eea62fcdf"
